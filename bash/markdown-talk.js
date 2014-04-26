55d071388bfc5f797e067f3e72a468d3e6ba9360
Shawn Biddle <shawncplus@gmail.com>
2014-04-26 17:29:35 -0400

    fixin stuff

--- bash/markdown-talk.js
+++ bash/markdown-talk.js
@@ -1,45 +1,41 @@
-Element.prototype.documentOffsetTop = function ()
+$(function ()
 {
-	return this.offsetTop + ( this.offsetParent ? this.offsetParent.documentOffsetTop() : 0 );
-};
-
-var focusPoints = document.querySelectorAll('ol > li');
-var focusIndex = 0;
-focusPoints[focusIndex].classList.add('selected');
-document.onkeydown = function (e)
-{
-	if (e.keyCode !== 74 && e.keyCode !== 75 && e.keyCode !== 71)
+	var focusPoints = document.querySelectorAll('ol > li');
+	var focusIndex = 0;
+	focusPoints[focusIndex].classList.add('selected');
+	document.onkeydown = function (e)
 	{
-		return;
-	}
+		if (e.keyCode !== 74 && e.keyCode !== 75 && e.keyCode !== 71)
+		{
+			return;
+		}
 
-	if (e.keyCode === 71)
-	{
-		window.scrollTo(0, 0);
-		focusPoints[focusIndex].classList.remove('selected');
-		focusIndex = 0;
-		focusPoints[focusIndex].classList.add('selected');
-		return;
-	}
+		if (e.keyCode === 71)
+		{
+			window.scrollTo(0, 0);
+			focusPoints[focusIndex].classList.remove('selected');
+			focusIndex = 0;
+			focusPoints[focusIndex].classList.add('selected');
+			return;
+		}
 
-	var direction = e.keyCode === 74 ? 1 : -1;
+		var direction = e.keyCode === 74 ? 1 : -1;
 
-	if (focusPoints[focusIndex + direction])
-	{
-		focusPoints[focusIndex].classList.remove('selected');
-		focusIndex += direction;
-		var focusPoint = focusPoints[focusIndex];
-		focusPoint.classList.add('selected');
+		if (focusPoints[focusIndex + direction])
+		{
+			focusPoints[focusIndex].classList.remove('selected');
+			focusIndex += direction;
+			var focusPoint = focusPoints[focusIndex];
+			focusPoint.classList.add('selected');
 
-		var offset
-		var top = focusPoint.documentOffsetTop() - ( window.innerHeight / 2 ) + (focusPoint.offsetHeight / 2);
+			var top = focusPoint.offsetTop - ( window.innerHeight / 2 ) + (focusPoint.offsetHeight / 2);
+			window.scrollTo(0, Math.min(top, focusPoint.offsetTop));
+		}
+	};
 
-		window.scrollTo(0, Math.min(top, focusPoint.offsetTop));
-	}
-};
+	var subtitle = document.createElement('div');
+	subtitle.innerHTML = document.querySelector('subtitle').innerHTML;
+	subtitle.classList.add('subtitle');
 
-var subtitle = document.createElement('div');
-subtitle.innerHTML = document.querySelector('subtitle').innerHTML;
-subtitle.classList.add('subtitle');
-
-document.querySelector('div.container').appendChild(subtitle);
+	document.querySelector('div.container').appendChild(subtitle);
+});
